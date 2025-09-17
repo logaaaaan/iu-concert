7f59749476655146d14d0c298396e23acca4fe91
logan <loganjohnen@gmail.com>
2025-09-17 18:20:15 +0200

    adjust descriptions

--- src/main/resources/sql/script.sql
+++ src/main/resources/sql/script.sql
@@ -96,10 +96,10 @@ VALUES
 -- Insert sample data into bands
 INSERT INTO band (bandName, cover, short_desc, description, genre)
 VALUES
-    ('Metallica', 'https://t4.ftcdn.net/jpg/09/07/89/09/360_F_907890951_ro0QZIF3dFqnxn7hiL19FNLL96uU7HhD.jpg', 'Heavy Metal Legends', 'Die legendären Heavy Metal Titanen Metallica', '["Heavy Metal", "Thrash Metal"]'),
-    ('Taylor Swift', 'https://cdn.shopify.com/s/files/1/0594/4639/5086/files/Taylor_Swift_Autograph_49ae9544-0be9-410c-810d-9517437bcc98.webp?v=1747155069', 'Pop Superstar', 'Taylor Swift ist eine US-amerikanische Singer-Songwriterin', '["Pop", "Country"]'),
-    ('Rammstein', 'https://www.webwandtattoo.com/de/img/asmu288-jpg/folder/products-listado-merchant/aufkleber-rammstein-emblem-.jpg', 'Industrial Metal', 'Deutsche Industrial Metal Band', '["Industrial Metal", "Neue Deutsche Härte"]'),
-    ('Ed Sheeran', 'https://2wjyoqczplq2loncx0jfrnk1q5w-thumbnail.storage.muc1.de.bnerd.com/thumbnail/de/Veranstaltungen/2025/Ed%20Sheeran/10955/image-thumb__10955__thumbnail-event-detail-hero/2025_03_07_ED%20SHEERAN_Shot09_034%20copy%201.2f4e5768.webp', 'Singer-Songwriter', 'Britischer Singer-Songwriter', '["Pop", "Folk"]');
+    ('Metallica', 'https://t4.ftcdn.net/jpg/09/07/89/09/360_F_907890951_ro0QZIF3dFqnxn7hiL19FNLL96uU7HhD.jpg', 'Heavy Metal Legends', 'Die Metal-Legenden live auf der Bühne! Erwartet euch eine donnernde Show mit Klassikern wie ''Enter Sandman'', ''Master of Puppets'' und neuen Hits. Pyrotechnik und Lichtshow inklusive!', '["Heavy Metal", "Thrash Metal"]'),
+    ('Taylor Swift', 'https://cdn.shopify.com/s/files/1/0594/4639/5086/files/Taylor_Swift_Autograph_49ae9544-0be9-410c-810d-9517437bcc98.webp?v=1747155069', 'Pop Superstar', 'Erlebe Taylor Swifts ikonische ''Eras Tour'' mit Hits aus allen ihren Alben. Eine spektakuläre Show mit atemberaubenden Bühnenbildern, Kostümwechseln und über 3 Stunden voller musikalischer Highlights.', '["Pop", "Country"]'),
+    ('Rammstein', 'https://www.webwandtattoo.com/de/img/asmu288-jpg/folder/products-listado-merchant/aufkleber-rammstein-emblem-.jpg', 'Industrial Metal', 'Feuer, Pyrotechnik und deutsche Lyrik - das typische Rammstein-Erlebnis. Eine der spektakulärsten Live-Shows der Welt mit atemberaubenden Effekten und mitreißender Energie.', '["Industrial Metal", "Neue Deutsche Härte"]'),
+    ('Ed Sheeran', 'https://2wjyoqczplq2loncx0jfrnk1q5w-thumbnail.storage.muc1.de.bnerd.com/thumbnail/de/Veranstaltungen/2025/Ed%20Sheeran/10955/image-thumb__10955__thumbnail-event-detail-hero/2025_03_07_ED%20SHEERAN_Shot09_034%20copy%201.2f4e5768.webp', 'Singer-Songwriter', 'Intim und doch grandios: Ed Sheeran bezaubert mit seiner Loop-Station und seiner charismatischen Bühnenpräsenz. Perfekt für einen unvergesslichen Akustik-Abend.', '["Pop", "Folk"]');
 
 -- Insert sample data into concerts (Termine)
 INSERT INTO concert (bandId, venueId, duration, concertDate, concertTime, price, vipPrice, standingSeatsSold, seatingSeatsSold, vipSeatsSold)
